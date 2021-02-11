2c33414101e420993be9e05c37d8a4b24be7b23d
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-11 16:06:54 +0200

    *added service layer with an ItemService class and a method for creating an Item

---
+++ src/main/java/com/example/trackyourthings/service/ItemService.java
@@ -0,0 +1,33 @@
+package com.example.trackyourthings.service;
+
+import com.example.trackyourthings.domain.Item;
+import com.example.trackyourthings.persistence.ItemRepository;
+import com.example.trackyourthings.transfer.CreateItemRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ItemService.class);
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public Item createItem(CreateItemRequest request) {
+
+        LOGGER.info("Creating item: {}", request);
+
+        Item createdItem = new Item();
+        BeanUtils.copyProperties(request, createdItem);
+
+        return itemRepository.save(createdItem);
+    }
+}
