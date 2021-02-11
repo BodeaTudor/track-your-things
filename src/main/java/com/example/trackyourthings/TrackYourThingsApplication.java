961d47a240e95954d63cfcaad45ab165e128e929
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-11 15:52:43 +0200

    Initial commit

---
+++ src/main/java/com/example/trackyourthings/TrackYourThingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.trackyourthings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackYourThingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackYourThingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trackyourthings/TrackYourThingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trackyourthings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackYourThingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
