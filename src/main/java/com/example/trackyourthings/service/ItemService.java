031d03ad34f009d097194a0d3e81b43521932404
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-11 16:22:17 +0200

    *added getItems method in ItemService for retrieving ALL items

--- src/main/java/com/example/trackyourthings/service/ItemService.java
+++ src/main/java/com/example/trackyourthings/service/ItemService.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -29,5 +31,12 @@ public class ItemService {
         BeanUtils.copyProperties(request, createdItem);
 
         return itemRepository.save(createdItem);
+    }
+
+    public Page<Item> getItems(Pageable pageable) {
+
+        LOGGER.info("Retrieving items...");
+
+        return itemRepository.findAll(pageable);
     }
 }
